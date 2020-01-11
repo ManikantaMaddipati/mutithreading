8be1a339761ae82f747c4698411309ea6d7866c0
manikanta <manikanta3517@gmail.com>
2020-01-10 19:07:05 -0500

    created my first thread group

--- src/main/java/MyFirstThread.java
+++ src/main/java/MyFirstThread.java
@@ -3,6 +3,13 @@ public class MyFirstThread
     public static void main(String[] args) {
         int i =2;
         Thread t = new Mythread();
+        //thread group: an interrupt , all the thread of that group are stopped.
+//              thread group gets callback saying that one of the thread died due to exception.
+//By Default current thread group becomes the thread.
+//To create thread group :
+        ThreadGroup group = new ThreadGroup("mygroup") ;
+//To assign thread to that a group
+        Thread xyz = new Thread(group,t);
         t.start();
         for(int k = 1; k<=10;k++){
             System.out.println(k*i);
