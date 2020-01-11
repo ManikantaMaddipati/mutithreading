262d0bba16f846796d24701b87cd1541df58aae4
manikanta <manikanta3517@gmail.com>
2020-01-10 21:11:32 -0500

    Created BankAccount and Two Threads but without any RaceConditions

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount
+{
+    private double balance;
+    public BankAccount(double initial){
+        balance=initial;
+    }
+
+    public void deposit(double amount){
+        balance = balance+amount;
+    }
+
+    public void withdrawal(double amount){
+        deposit(-amount);
+    }
+}
--- src/main/java/MyFirstThread.java
+++ src/main/java/MyFirstThread.java
@@ -1,28 +1,33 @@
 public class MyFirstThread
 {
     public static void main(String[] args) {
-        int i =2;
-        Thread t = new Mythread();
-        //thread group: an interrupt , all the thread of that group are stopped.
-//              thread group gets callback saying that one of the thread died due to exception.
-//By Default current thread group becomes the thread.
-//To create thread group :
-        ThreadGroup group = new ThreadGroup("mygroup") ;
-//To assign thread to that a group
-        Thread xyz = new Thread(group,t);
-        t.start();
-        for(int k = 1; k<=10;k++){
-            System.out.println(k*i);
-        }
+        BankAccount bankAccount = new BankAccount(1000);
+
+        Thread t1 = new MythreadMoney1(bankAccount);
+        Thread t2 = new MythreadMoney2(bankAccount);
+        t1.start();
+        t2.start();
     }
 }
 
-class Mythread extends Thread{
+class MythreadMoney1 extends Thread{
+    BankAccount bankAccount;
+    MythreadMoney1(BankAccount bankAccount){
+        this.bankAccount=bankAccount;
+    }
     public void run(){
-        int i =3;
-        for(int k = 1; k<=10;k++){
-            System.out.println(k*i);
-        }
+        bankAccount.deposit(100);
     }
 }
+
+class MythreadMoney2 extends Thread{
+    BankAccount bankAccount;
+    MythreadMoney2(BankAccount bankAccount){
+        this.bankAccount=bankAccount;
+    }
+    public void run(){
+        bankAccount.withdrawal(100);
+    }
+}
+
 
