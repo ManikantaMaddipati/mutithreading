1a5035acc09130de2f74a55b0375eaf1aef201c3
manikanta <mm989@njit.edu>
2020-02-20 05:13:05 -0500

    Timer and  Task scheduler

--- src/main/java/com/manikanta/MyFirstThread.java
+++ src/main/java/com/manikanta/MyFirstThread.java
@@ -1,45 +1,56 @@
 package com.manikanta;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyFirstThread {
     public static void main(String[] args) {
         LinkList linkList = new LinkList();
 
         Thread t1 = new MythreadMoney1(linkList);
-//        Thread t2 = new MythreadMoney2(linkList);
+        Thread t2 = new MythreadMoney2(linkList);
         t1.start();
-  //      t2.start();
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                t2.start();
+            }
+        };
+        timer.schedule(task, 0, 1000);
+
+
     }
-//        LinkList linkList = new LinkList();
-//        for (int i =0 ; i<100; i++) {
-//            linkList.append(i);
-//        }
-//        linkList.printList();
-//
 }
 
-class MythreadMoney1 extends Thread{
+
+class MythreadMoney1 extends Thread {
     LinkList linkList1;
-    MythreadMoney1(LinkList linkList1){
-        this.linkList1=linkList1;
+
+    MythreadMoney1(LinkList linkList1) {
+        this.linkList1 = linkList1;
     }
-    public void run(){
-        for (int i =1; i<500; i++) {
+
+    public void run() {
+        for (int i = 1; i < 500; i++) {
             linkList1.append(i);
         }
 
-        linkList1.printList();
-
     }
 }
 
 
-//class MythreadMoney2 extends Thread{
-//    LinkList linkList2;
-//    MythreadMoney2(LinkList linkList2){
-//        this.linkList2=linkList2;
-//    }
-//    public void run(){
-//        linkList2.printList();
-//    }
+class MythreadMoney2 extends Thread {
+    LinkList linkList2;
+
+    MythreadMoney2(LinkList linkList2) {
+        this.linkList2 = linkList2;
+    }
+
+    public void run() {
+        linkList2.printList();
+    }
+}
 
 
